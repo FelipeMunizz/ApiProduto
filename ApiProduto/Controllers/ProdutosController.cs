e6d8cec0609f895b68c1a6401bc2788f3f91b2cf
Felipe Muniz <lipe.baterra@gmail.com>
2022-08-05 10:52:05 -0300

    Adicionar arquivos de projeto.

---
+++ ApiProduto/Controllers/ProdutosController.cs
@@ -0,0 +1,29 @@
+﻿using ApiProduto.Data;
+using ApiProduto.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProduto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> Get()
+        {
+            if(_context.Produtos is null)
+            {
+                return NotFound("Nunhum Produto Encontrado");
+            }
+            return await _context.Produtos.ToListAsync();
+        }
+    }
+}
---
+++ ApiProduto/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiProduto.Models;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+
+    [Required(ErrorMessage = "Digite o Nome do Produto")]
+    [StringLength(100, ErrorMessage = "Nome não pode ter mais que 100 caracteres")]
+    public string? Nome { get; set; }
+
+    [Required(ErrorMessage = "Digite o Preço")]
+    [Range(1, 999.99)]
+    public double Preco { get; set; }
+
+    [Required(ErrorMessage = "Digite a Descrição do Produto")]
+    [StringLength(200, ErrorMessage = "A descrição é de no maximo 200 caracteres")]
+    public string? Descricao { get; set; }
+    public DateTime DataCadastro { get; set; }
+}
